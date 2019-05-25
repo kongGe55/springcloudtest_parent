fd08db539277fcf0704ddd9ffe5618452da65e55
Ji Liugang <905459569@qq.com>
2019-05-25 09:56:47 +0800

    Initial commit

---
+++ eureka_erver/src/main/java/com/example/eureka_erver/EurekaErverApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_erver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaErverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaErverApplication.class, args);
+    }
+
+}
