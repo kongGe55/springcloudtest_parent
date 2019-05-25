988f9652e20dbb3163d2277f2056fdfe8837294b
Ji Liugang <905459569@qq.com>
2019-05-25 10:28:13 +0800

    secound

--- eureka_erver/src/main/java/com/example/eureka_erver/EurekaErverApplication.java
+++ eureka_erver/src/main/java/com/example/eureka_erver/EurekaErverApplication.java
@@ -2,8 +2,10 @@ package com.example.eureka_erver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaErverApplication {
 
     public static void main(String[] args) {
